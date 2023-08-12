b7d5a9ef5c9300e1fa111db95dcd27f939912365
DanceMore <dancemore@protonmail.com>
2023-08-12 11:16:54 -0700

    more refactoring

--- src/kodi_helper.rs
+++ src/kodi_helper.rs
@@ -4,6 +4,8 @@ use reqwest::Client;
 use reqwest::header::{HeaderValue, HeaderMap};
 use std::error::Error;
 use std::fs;
+use rand::prelude::SliceRandom;
+use serde_json::json;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -52,7 +54,6 @@ pub async fn rpc_call(base_url: &str, auth: &Authorization, request_params: &Val
         .send()
         .await?;
 
-
     // Read response body as bytes and deserialize using serde_json
     let response_bytes: Vec<u8> = response.bytes().await?.to_vec();
     let response_str = String::from_utf8_lossy(&response_bytes);
@@ -63,3 +64,152 @@ pub async fn rpc_call(base_url: &str, auth: &Authorization, request_params: &Val
         Err(err) => Err(Box::new(err)), // Wrap the error in a Box
     }
 }
+
+// Episode related RPC Calls
+pub struct SelectedEpisode {
+    episode_id: u64,
+    episode_file_path: String,
+}
+
+
+// Define a struct that holds configuration and authorization
+pub struct RpcClient {
+    pub config: Config,
+    pub auth: Authorization,
+}
+
+impl RpcClient {
+    // Create a new instance of RpcClient
+    pub fn new(config: Config) -> Result<Self, Box<dyn Error>> {
+        let auth = Authorization::new(&config.username, &config.password);
+        Ok(RpcClient { auth, config })
+    }
+
+    pub async fn select_random_episode_by_title(
+	&self, tv_show_name: &str,
+    ) -> Result<SelectedEpisode, Box<dyn Error>> {
+        // Fetch the list of TV shows
+        let tv_shows_request_params = json!({
+    	    "jsonrpc": "2.0",
+    	    "method": "VideoLibrary.GetTVShows",
+    	    "params": {
+    		    "properties": ["title"],
+    		    "limits": { "start": 0, "end": 1000 }
+    	    },
+    	    "id": 1
+        });
+
+        let tv_shows_response_json = rpc_call(&self.config.url, &self.auth, &tv_shows_request_params).await?;
+
+        // Extract the "tvshows" array from the "result" field
+        let tv_shows = tv_shows_response_json["result"]["tvshows"]
+    	    .as_array()
+    	    .ok_or("TV shows not found in response")?;
+
+        // Find the TV show with the given name
+        let tv_show = tv_shows
+    	    .iter()
+    	    .find(|show| show["title"].as_str() == Some(tv_show_name))
+    	    .ok_or_else(|| format!("TV show {} not found", tv_show_name))?;
+
+        println!("Selected TV Show: {:?}", tv_show);
+
+        let tv_show_id = tv_show["tvshowid"].as_u64().ok_or("TV show ID not found")?;
+        println!("Selected TV Show ID: {}", tv_show_id);
+
+        // Fetch the list of episodes
+        let episodes_request_params = json!({
+    	    "jsonrpc": "2.0",
+    	    "method": "VideoLibrary.GetEpisodes",
+    	    "params": {
+    		    "tvshowid": tv_show_id, // Use the TV show ID you obtained earlier
+    		    "properties": ["title", "season", "episode"],
+    		    "limits": { "start": 0, "end": 1000 }
+    	    },
+    	    "id": 1
+        });
+
+        let episodes_response_json = rpc_call(&self.config.url, &self.auth, &episodes_request_params).await?;
+
+        //println!("Episodes Response: {:?}", episodes_response_json);
+
+        // Extract the "episodes" array from the "result" field
+        let episodes = episodes_response_json["result"]["episodes"]
+    	    .as_array()
+    	    .ok_or("Episodes not found in response")?;
+
+        //for episode in episodes {
+        //        let episode_id = episode["episodeid"].as_u64().ok_or("Episode ID not found")?;
+        //        let episode_title = episode["title"].as_str().ok_or("Episode title not found")?;
+        //        let season_number = episode["season"].as_u64().ok_or("Season number not found")?;
+        //        let episode_number = episode["episode"].as_u64().ok_or("Episode number not found")?;
+
+        //        println!(
+        //    	    "Episode ID: {}, Title: {}, Season: {}, Episode: {}",
+        //    	    episode_id, episode_title, season_number, episode_number
+        //    	    );
+        //}
+
+        // Extract the episode IDs from the episodes array
+        let episode_ids: Vec<u64> = episodes
+    	    .iter()
+    	    .map(|episode| episode["episodeid"].as_u64().unwrap())
+    	    .collect();
+
+        // Randomly select an episode ID
+        let mut rng = rand::thread_rng();
+        let random_episode_id = episode_ids.choose(&mut rng).ok_or("No episodes available")?;
+
+        //println!("Randomly selected episode ID: {:?}", random_episode_id);
+
+        // Prepare the request parameters
+        let episode_details_request_params = json!({
+    	    "jsonrpc": "2.0",
+    	    "method": "VideoLibrary.GetEpisodeDetails",
+    	    "params": {
+    		    "episodeid": random_episode_id,
+    		    "properties": ["file"] // You can also include other properties you need
+    	    },
+    	    "id": 1
+        });
+
+        // Make the RPC call
+        let episode_details_response_json = rpc_call(&self.config.url, &self.auth, &episode_details_request_params).await?;
+
+        // Extract the episode file path from the response
+        let episode_file_path = episode_details_response_json["result"]["episodedetails"]["file"]
+            .as_str()
+            .ok_or("Episode file path not found in response")?
+            .to_string(); // Convert to String
+
+        //println!("[!] file path => {:?}", episode_file_path);
+
+        let selected_episode = SelectedEpisode {
+            episode_id: *random_episode_id,
+            episode_file_path,
+        };
+
+        Ok(selected_episode)
+    }
+
+    // Method to make an RPC call for playing an episode
+    pub async fn rpc_play(&self, selected_episode: &SelectedEpisode) -> Result<(), Box<dyn Error>> {
+        let play_episode_request_params = json!({
+            "jsonrpc": "2.0",
+            "method": "Player.Open",
+            "params": {
+                "item": {
+                    "file": &selected_episode.episode_file_path,
+                }
+            },
+            "id": 1
+        });
+
+        // Make the RPC call to play the episode
+        let _play_response = rpc_call(&self.config.url, &self.auth, &play_episode_request_params).await?;
+        println!("Play response: {:?}", _play_response);
+
+        Ok(())
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 mod kodi_helper;
 use kodi_helper::Config;
-use kodi_helper::Authorization;
-use kodi_helper::rpc_call;
+use kodi_helper::RpcClient;
 
-use serde_json::{json};
 use std::error::Error;
 use std::env;
-use rand::prelude::SliceRandom;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // Load configuration from YAML
     let config = Config::load("config.yml")?;
-    let base_url = &config.url;
-    let auth = Authorization::new(&config.username, &config.password);
 
     // Get the TV show name from the command-line arguments
     let args: Vec<String> = env::args().collect();
@@ -21,121 +16,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	    eprintln!("Usage: {} <TV Show Name>", args[0]);
 	    return Ok(());
     }
-    let tv_show_name = &args[1];
 
+    let tv_show_name = &args[1];
     println!("[-] target => {:?}", tv_show_name);
 
-    // Fetch the list of TV shows
-    let tv_shows_request_params = json!({
-	    "jsonrpc": "2.0",
-	    "method": "VideoLibrary.GetTVShows",
-	    "params": {
-		    "properties": ["title"],
-		    "limits": { "start": 0, "end": 1000 }
-	    },
-	    "id": 1
-    });
+    // build RPC client and run
+    let rpc_client = RpcClient::new(config)?;
 
-    let tv_shows_response_json = rpc_call(&base_url, &auth, &tv_shows_request_params).await?;
+    // Call the function to select a random episode by title
+    let selected_episode = rpc_client.select_random_episode_by_title(tv_show_name).await?;
 
-    // Extract the "tvshows" array from the "result" field
-    let tv_shows = tv_shows_response_json["result"]["tvshows"]
-	    .as_array()
-	    .ok_or("TV shows not found in response")?;
-
-    // Find the TV show with the given name
-    let tv_show = tv_shows
-	    .iter()
-	    .find(|show| show["title"].as_str() == Some(tv_show_name))
-	    .ok_or_else(|| format!("TV show {} not found", tv_show_name))?;
-
-    println!("Selected TV Show: {:?}", tv_show);
-
-    let tv_show_id = tv_show["tvshowid"].as_u64().ok_or("TV show ID not found")?;
-    println!("Selected TV Show ID: {}", tv_show_id);
-
-    // Fetch the list of episodes
-    let episodes_request_params = json!({
-	    "jsonrpc": "2.0",
-	    "method": "VideoLibrary.GetEpisodes",
-	    "params": {
-		    "tvshowid": tv_show_id, // Use the TV show ID you obtained earlier
-		    "properties": ["title", "season", "episode"],
-		    "limits": { "start": 0, "end": 1000 }
-	    },
-	    "id": 1
-    });
-
-    let episodes_response_json = rpc_call(&base_url, &auth, &episodes_request_params).await?;
-
-    //println!("Episodes Response: {:?}", episodes_response_json);
-
-    // Extract the "episodes" array from the "result" field
-    let episodes = episodes_response_json["result"]["episodes"]
-	    .as_array()
-	    .ok_or("Episodes not found in response")?;
-
-    //for episode in episodes {
-    //        let episode_id = episode["episodeid"].as_u64().ok_or("Episode ID not found")?;
-    //        let episode_title = episode["title"].as_str().ok_or("Episode title not found")?;
-    //        let season_number = episode["season"].as_u64().ok_or("Season number not found")?;
-    //        let episode_number = episode["episode"].as_u64().ok_or("Episode number not found")?;
-
-    //        println!(
-    //    	    "Episode ID: {}, Title: {}, Season: {}, Episode: {}",
-    //    	    episode_id, episode_title, season_number, episode_number
-    //    	    );
-    //}
-
-    // Extract the episode IDs from the episodes array
-    let episode_ids: Vec<u64> = episodes
-	    .iter()
-	    .map(|episode| episode["episodeid"].as_u64().unwrap())
-	    .collect();
-
-    // Randomly select an episode ID
-    let mut rng = rand::thread_rng();
-    let random_episode_id = episode_ids.choose(&mut rng).ok_or("No episodes available")?;
-
-    println!("Randomly selected episode ID: {:?}", random_episode_id);
-
-    // Prepare the request parameters
-    let episode_details_request_params = json!({
-	    "jsonrpc": "2.0",
-	    "method": "VideoLibrary.GetEpisodeDetails",
-	    "params": {
-		    "episodeid": random_episode_id,
-		    "properties": ["file"] // You can also include other properties you need
-	    },
-	    "id": 1
-    });
-
-    // Make the RPC call
-    let episode_details_response_json = rpc_call(&base_url, &auth, &episode_details_request_params).await?;
-
-    // Extract the file path from the response
-    let episode_file_path = episode_details_response_json["result"]["episodedetails"]["file"]
-	    .as_str()
-	    .ok_or("Episode file path not found in response")?;
-
-    println!("[!] file path => {:?}", episode_file_path);
-
-
-    // Prepare the request parameters for playing the episode
-    let play_episode_request_params = json!({
-	    "jsonrpc": "2.0",
-	    "method": "Player.Open",
-	    "params": {
-		    "item": {
-			    "file": episode_file_path
-		    }
-	    },
-	    "id": 1
-    });
+    // Call the function to play the selected episode
+    rpc_client.rpc_play(&selected_episode).await?;
 
     // Make the RPC call to play the episode
-    let play_response = rpc_call(&base_url, &auth, &play_episode_request_params).await?;
-    println!("Play response: {:?}", play_response);
+//    let play_response = rpc_call(&base_url, &auth, &play_episode_request_params).await?;
+//    println!("Play response: {:?}", play_response);
 
     Ok(())
 }
